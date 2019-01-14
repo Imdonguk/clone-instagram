f3dc57b8b15e7bcfc6702b7e2ed4dbc508cddbfc
Imdonguk <com8157@naver.com>
2019-01-14 12:28:16 +0900

    Add : Header스타일적용 및 Account, Input, Logo로 컴포넌트 분류

---
+++ src/Header/Header.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Logo from './Logo'
+import Input from './Input'
+import Accounts from './Accounts'
+import styled from 'styled-components';
+
+class Header extends Component {
+    render() {
+        return (
+            <HeaderWrap>
+                <Content>
+                    <Logo></Logo>
+                    <Input></Input>
+                    <Accounts></Accounts>
+                </Content>
+            </HeaderWrap>
+        );
+    }
+}
+
+const HeaderWrap = styled.div`
+    width : 100%;
+    height : 7.7rem;
+    display : flex;
+    justify-content: center;
+    border-bottom : 0.05rem solid #ddd;
+`
+
+const Content = styled.div`
+    width : 101rem; 
+    height : 100%;
+    padding : 0 2rem;
+    display : flex;
+    align-items : center;
+    box-sizing : border-box;
+    justify-content : space-between;
+`
+
+export default Header;
