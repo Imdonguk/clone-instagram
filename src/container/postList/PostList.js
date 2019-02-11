522eea185f7cd8a2331647b99102d81236e9a8b1
Imdonguk <com8157@naver.com>
2019-02-11 19:35:54 +0900

    Add : PostForm과 SettingForm에 props전달

--- src/container/postList/PostList.js
+++ src/container/postList/PostList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import Post from './post/Post'
 import PostForm from './postForm/PostForm'
+import SettingForm from './settingForm/SettingForm'
 import _ from 'lodash'
 
 class PostList extends Component {
@@ -56,25 +57,32 @@ class PostList extends Component {
     }
 
     render() {
-        const { bePostForm } = this.props;
-        const { information } = this.state;
+        const { bePostForm, isPostForm } = this.props;
+        const { information, beSettingForm, postNo } = this.state;
         const postList = information.map((info, idx) => (
             <Post
                 info={info}
                 key={idx}
                 index={idx}
                 addComment={this.addComment}
+                isSettingForm={this.isSettingForm}
             />
         ))
+
         return (
             <PostWrap>
                 {postList}
-                {bePostForm ?
-                    <PostForm
-                        isPostForm={this.props.isPostForm}
-                        addPost={this.addPost}
-                    /> : ''
-                }
+                {bePostForm && <PostForm
+                    isPostForm={isPostForm}
+                    addPost={this.addPost}
+                    editPost={this.editPost}
+                    mode={postNo || postNo === 0 ? 'edit' : 'add'}
+                />}
+                {beSettingForm && <SettingForm
+                    isPostForm={isPostForm}
+                    isSettingForm={this.isSettingForm}
+                    removePost={this.removePost}
+                />}
             </PostWrap>
         );
     }
