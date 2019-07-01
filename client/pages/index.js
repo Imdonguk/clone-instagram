1709e62ad0802f86db058bd790d2ba36035de816
Imdonguk <com8157@naver.com>
2019-07-01 16:02:01 +0900

    Setting : react, next 설치와 eslint, prettier등 코드컨벤션 세팅

---
+++ client/pages/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Index = () => {
+  return <>리뉴얼 인스타그램!!</>
+}
+export default Index
---
+++ client/pages/signin.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Signin = () => {
+  return <>로그인페이지</>
+}
+
+export default Signin
