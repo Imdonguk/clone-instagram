6526f5a98a51fabe143cb0f5759baae2b1f7efec
Imdonguk <com8157@naver.com>
2019-07-18 14:58:30 +0900

    Install & Setting : redux설치 및 react와 redux 연결

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './default.css'
+import withRedux from 'next-redux-wrapper'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from '../reducers'
 
-const App = ({ Component }) => {
+const App = ({ Component, store }) => {
   return (
-    <>
+    <Provider store={store}>
       <Component />
-    </>
+    </Provider>
   )
 }
 
@@ -14,4 +18,7 @@ App.propTypes = {
   Component: PropTypes.elementType.isRequired,
 }
 
-export default App
+export default withRedux((initialState, options) => {
+  const store = createStore(reducer, initialState)
+  return store
+})(App)
