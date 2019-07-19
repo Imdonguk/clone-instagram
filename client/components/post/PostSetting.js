3a845b991248c7db7ab73665cf7be919a61aeba2
Imdonguk <com8157@naver.com>
2019-07-19 15:15:17 +0900

    Add : 게시물을 변경할 수 있는 수정,삭제 UI 추가

---
+++ client/components/post/PostSetting.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Wrap } from './PostForm'
+
+const PostSetting = () => {
+  return (
+    <Wrap>
+      <Box>
+        <EditButton>수정하기</EditButton>
+        <RemoveButton>제거하기</RemoveButton>
+      </Box>
+    </Wrap>
+  )
+}
+
+const Box = styled.div`
+  width: 40rem;
+  background-color: #fff;
+  border-radius: 2rem;
+`
+
+const BasicButton = styled.button`
+  width: 100%;
+  height: 7rem;
+  font-size: 1.6rem;
+  font-weight: 600;
+  outline: none;
+  cursor: pointer;
+`
+
+const EditButton = styled(BasicButton)`
+  color: #3897f0;
+  border-top-left-radius: 2rem;
+  border-top-right-radius: 2rem;
+`
+
+const RemoveButton = styled(BasicButton)`
+  color: #ed4956;
+  border-bottom-left-radius: 2rem;
+  border-bottom-right-radius: 2rem;
+`
+
+export default PostSetting
