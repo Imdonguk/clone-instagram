c7e9fef277d0f94067ad9d5773a0b9ccf47ad9c4
Imdonguk <com8157@naver.com>
2019-07-25 21:40:05 +0900

    Modify : 댓글컴포넌트 따로 분리

---
+++ client/components/post/PostComment.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const PostComment = ({ id, name, comment }) => {
+  return (
+    <Item>
+      <Name>{name}</Name>
+      {comment}
+    </Item>
+  )
+}
+
+const Item = styled.div`
+  width: 100%;
+  font-size: 1.4rem;
+  margin-bottom: 1rem;
+`
+const Name = styled.span`
+  margin-right: 0.5rem;
+  font-weight: 800;
+`
+
+export default PostComment
