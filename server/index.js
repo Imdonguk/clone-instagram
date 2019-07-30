a8d77c137046244457f694fa5345cd96c3a05c53
Imdonguk <com8157@naver.com>
2019-07-30 23:59:48 +0900

    Add : mysql설치 및 models모듈에 index.js파일 추가

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require('express')
-
+const morgan = require('morgan')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const expressSession = require('express-session')
 const app = express()
 
-app.listen(8080, () => {
-  console.log('start server')
-})
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(expressSession({
+
+}))
+app.use(cors())
+app.listen(3000, () => {})
