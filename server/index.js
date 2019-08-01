5170ca9f467d1c09973919f9fdce7645c53f957b
Imdonguk <com8157@naver.com>
2019-08-01 23:54:06 +0900

    Add : 디비연결

--- server/index.js
+++ server/index.js
@@ -4,13 +4,16 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const expressSession = require('express-session')
 const app = express()
+const db = require('./models')
+
+db.sequelize.sync()
 
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(expressSession({
-
-}))
+// app.use(expressSession({}))
 app.use(cors())
-app.listen(3000, () => {})
+app.listen(3000, () => {
+  console.log('start server')
+})
