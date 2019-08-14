6238a69193a2f3567d370999d7b65c1856da82b7
Imdonguk <com8157@naver.com>
2019-08-14 22:20:14 +0900

    Add : 클라이언트에서의 로그인동작

--- client/pages/signin.js
+++ client/pages/signin.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useDispatch } from 'react-redux'
 import { AccountWrap, Content, LetterIcon, HiperLink, AppDown } from '../components/account'
+import { SIGN_IN_REQUEST } from '../reducers/user'
 
 const Signin = () => {
+  const userNameRef = useRef('')
+  const passwordRef = useRef('')
+  const dispatch = useDispatch()
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const data = {
+      userName: userNameRef.current.value,
+      password: passwordRef.current.value,
+    }
+    dispatch({
+      type: SIGN_IN_REQUEST,
+      data,
+    })
+  }
   return (
     <AccountWrap>
       <Content>
         <div className="box">
           <LetterIcon />
-          <form>
-            <input type="text" name="userName" placeholder="사용자이름" required />
-            <input type="password" name="password" placeholder="패스워드" required />
+          <form onSubmit={handleSubmit}>
+            <input type="text" name="userName" placeholder="사용자이름" ref={userNameRef} required />
+            <input type="password" name="password" placeholder="패스워드" ref={passwordRef} required />
             <button className="content-form-button" type="submit">
               로그인
             </button>
