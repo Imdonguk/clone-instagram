726f23425b337526d58e1550477b8708b6d3800a
Imdonguk <com8157@naver.com>
2019-08-18 18:59:59 +0900

    Add : 메인페이지에 프로필UI구현 및 reducer state 변경

--- client/components/Container.js
+++ client/components/Container.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useSelector } from 'react-redux'
+import Link from 'next/link'
 import { Post, PostForm, PostSetting } from './post'
 
 const Container = () => {
   const { posts, isPostSetting } = useSelector(state => state.post)
   const isPostForm = useSelector(state => state.user.isPostForm)
+  const { userName, nickName, profileImage } = useSelector(state => state.user.user)
+
   return (
     <Wrapper>
       <div className="container">
@@ -17,7 +20,21 @@ const Container = () => {
           {isPostSetting && <PostSetting />}
         </div>
         <div className="right">
-          <div className="woogie">ㅎㅎ</div>
+          <div className="profile">
+            <div className="profile-image">
+              <img src={profileImage} alt="프로필사진" />
+            </div>
+            <div className="profile-text">
+              <div className="user-name">
+                <Link href="/">
+                  <a>{userName}</a>
+                </Link>
+              </div>
+              <div className="nick-name">{nickName}</div>
+            </div>
+          </div>
+          <div className="story" />
+          <div className="recommend" />
         </div>
       </div>
     </Wrapper>
@@ -26,7 +43,7 @@ const Container = () => {
 
 const Wrapper = styled.div`
   min-height: 100vh;
-  & > .container {
+  .container {
     position: relative;
     display: grid;
     grid-template-columns: 61.4rem 1fr;
@@ -34,6 +51,43 @@ const Wrapper = styled.div`
     max-width: 93.5rem;
     min-height: 100%;
     grid-gap: 2.8rem;
+  }
+
+  .profile {
+    padding-left: 0.5rem;
+    margin-bottom: 1.2rem;
+    height: 5rem;
+    display: flex;
+    align-items: center;
+
+    &-image {
+      width: 5rem;
+      height: 5rem;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+      }
+    }
+
+    &-text {
+      margin-left: 1.4rem;
+      .user-name {
+        line-height: 1.8rem;
+        margin-bottom: 0.2rem;
+        font-weight: 600;
+        font-size: 1.6rem;
+
+        a {
+          text-decoration: none;
+          color: #262626;
+        }
+      }
+      .nick-name {
+        color: #999;
+        font-size: 1.2rem;
+      }
+    }
   }
 `
 
