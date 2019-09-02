70aaf71d4d496a6a95108bb520d96d95223b53d3
Imdonguk <com8157@naver.com>
2019-09-02 22:51:14 +0900

    Add : react-helmet을 사용해서 페이지의 head태그에 title,htmlAttributes,meta 태그 등 추가

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -6,15 +6,51 @@ import withReduxSaga from 'next-redux-saga'
 import { createStore, compose, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import createSagaMiddleware from 'redux-saga'
+import Helmet from 'react-helmet'
 import axios from 'axios'
 import rootSaga from '../sagas'
 import reducer from '../reducers'
 
 const WoogieBoogie = ({ Component, store, pageProps }) => {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+      <Provider store={store}>
+        <Helmet
+          title="clone-instagram"
+          htmlAttributes={{ lang: 'ko' }}
+          meta={[
+            {
+              charset: 'UTF-8',
+            },
+            {
+              name: 'viewport',
+              content:
+                'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover',
+            },
+            {
+              'http-equiv': 'X-UA-Compatible',
+              content: 'IE=edge',
+            },
+            {
+              name: 'description',
+              content: '우기부기의 SNS만들기',
+            },
+            {
+              name: 'og:title',
+              content: 'NodeBird',
+            },
+            {
+              name: 'og:description',
+              content: '우기부기의 SNS만들기',
+            },
+            {
+              property: 'og:type',
+              content: 'website',
+            },
+          ]}
+          link={[]}
+        />
+        <Component {...pageProps} />
+      </Provider>
   )
 }
 
