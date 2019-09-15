0735b33417201cc095652a279c0476b97df8dfa1
Imdonguk <com8157@naver.com>
2019-09-15 15:13:36 +0900

    Modify : passport를 사용하여 로그인인증 코드 재구현, 로그인시에 요청하는 api 응답코드 구현

--- server/index.js
+++ server/index.js
@@ -3,15 +3,17 @@ const morgan = require('morgan')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const expressSession = require('express-session')
-const passport = require('passport')
-const app = express()
+// const passport = require('passport')
 const db = require('./models')
 const dotenv = require('dotenv')
-const passportConfig = require('./passport')
+// const passportConfig = require('./passport')
+
+const FileStore = require('session-file-store')(expressSession)
 
 dotenv.config()
+const app = express()
 db.sequelize.sync()
-passportConfig()
+// passportConfig()
 
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false }))
@@ -26,8 +28,9 @@ app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(
   expressSession({
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     secret: process.env.COOKIE_SECRET,
+    store: new FileStore(),
     cookie: {
       httpOnly: true, //javascript로 쿠키나 세션 동작을 할 수 없게 하는 동작
       secure: false, // https를 쓸 때 true
@@ -35,11 +38,66 @@ app.use(
   }),
 )
 
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
 app.use(passport.initialize())
 app.use(passport.session())
 
+passport.serializeUser((user, done) => {
+  done(null, user.userName)
+})
+
+passport.deserializeUser(async (userName, done) => {
+  try {
+    const user = await db.User.findOne({
+      where: { userName },
+    })
+    done(null, user)
+  } catch (e) {}
+})
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'userName',
+      passwordField: 'password',
+    },
+    async (userName, password, done) => {
+      const user = await db.User.findOne({
+        where: { userName },
+      })
+      if (!user) return done(null, false, { message: '유저가 없습니다.' })
+      const validPassword = await bcrypt.compare(password, user.password)
+
+      if (!validPassword) return done(null, false, { message: '비밀번호가 틀립니다.' })
+      done(null, user)
+    },
+  ),
+)
+
+app.get('/user', (req, res, next) => {
+  if (!req.user) return res.json({ success: false, msg: '유저가없어욤' })
+  const user = Object.assign({}, req.user.toJSON())
+  delete user.password
+  res.json(user)
+})
+
+app.post('/signin', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) return next(err)
+    if (!user) return res.json({ success: false, msg: info.message })
+
+    return req.login(user, signinErr => {
+      return signinErr ? next(signinErr) : res.json({ success: true })
+    })
+  })(req, res, next)
+})
+
+app.post('/signout', (req, res, next) => {})
+
 app.use('/api/user', require('./routes/api/user'))
-app.use('/api/post', require('./routes/api/post'))
 
 app.listen(3065, () => {
   console.log('start server')
