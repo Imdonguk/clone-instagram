2580005f53bf70860cf54121d0f2967825659e90
Imdonguk <com8157@naver.com>
2019-10-28 17:43:01 +0900

    Add : 게시물의 좋아요버튼 눌렀을 때의 이벤트핸들러 추가

--- client/components/post/PostFooter.js
+++ client/components/post/PostFooter.js
@@ -1,18 +1,21 @@
-import React, { useRef } from 'react'
+import React, { useRef, useMemo, useCallback } from 'react'
 import styled from 'styled-components'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { LikeIcon, CommentIcon, SaveIcon, ShareIcon } from '../Icons'
-import { ADD_COMMENT } from '../../reducers/post'
+import { ADD_COMMENT, LIKE_POST_REQUEST, UNLIKE_POST_REQUEST } from '../../reducers/post'
 import { PostComment } from './index'
 
-const PostFooter = ({ id, des, comments, likeList, user }) => {
+const PostFooter = ({ postId, des, comments, likers, user }) => {
   const commentRef = useRef('')
   const dispatch = useDispatch()
+  const userId = useSelector(state => state.user.me && state.user.me.id)
+  const isLiked = useMemo(() => !!likers.find(v => v === userId), [likers])
+
   const addComment = e => {
     e.preventDefault()
     dispatch({
       type: ADD_COMMENT,
-      data: { id, user, comment: commentRef.current.value },
+      data: { postId, user, comment: commentRef.current.value },
     })
     commentRef.current.value = ''
   }
@@ -21,11 +24,25 @@ const PostFooter = ({ id, des, comments, likeList, user }) => {
     commentRef.current.focus()
   }
 
+  const handleClickLikeBtn = useCallback(async () => {
+    if (isLiked) {
+      dispatch({
+        type: UNLIKE_POST_REQUEST,
+        data: postId,
+      })
+    } else {
+      dispatch({
+        type: LIKE_POST_REQUEST,
+        data: postId,
+      })
+    }
+  }, [isLiked])
+
   return (
     <Wrap>
       <div className="icons">
         <IconWrap first>
-          <LikeIcon />
+          <LikeIcon like={isLiked} onClick={handleClickLikeBtn} />
         </IconWrap>
         <IconWrap>
           <CommentIcon onClick={handleClickCommentBtn} />
@@ -37,11 +54,11 @@ const PostFooter = ({ id, des, comments, likeList, user }) => {
           <SaveIcon />
         </IconWrap>
       </div>
-      <div className="like-user-count">좋아요 {likeList.length}개</div>
+      <div className="like-user-count">좋아요 {likers.length}개</div>
       <div className="comment">
         <PostComment name={user.name} comment={des} />
         {comments.map(v => (
-          <PostComment name={v.user.name} comment={v.comment} id={v.id} key={v.id.toString()} />
+          <PostComment name={v.user.name} comment={v.comment} id={v.id} />
         ))}
       </div>
       <div className="input-comment">
