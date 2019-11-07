e471373651bc4edcc616951b16120e9d03cc37c2
Imdonguk <com8157@naver.com>
2019-11-08 00:25:52 +0900

    Add : User페이지에 들어갈 때 발생하는 LOAD_OTHER_USER액션  사이클 추가

--- client/reducers/user.js
+++ client/reducers/user.js
@@ -2,6 +2,8 @@ import produce from 'immer'
 
 const initialState = {
   me: {},
+  userInfo: {},
+  isLoddingOwner: false,
 }
 
 export const SIGN_IN = 'SIGN_IN'
@@ -13,6 +15,10 @@ export const SIGN_OUT_FAILURE = 'SIGN_OUT_FAILURE'
 export const LOAD_USER_REQUEST = 'LOAD_USER_REQUEST'
 export const LOAD_USER_SUCCESS = 'LOAD_USER_SUCCESS'
 export const LOAD_USER_FAILURE = 'LOAD_USER_FAILURE'
+
+export const LOAD_OTHER_USER_REQUEST = 'LOAD_OTHER_USER_REQUEST'
+export const LOAD_OTHER_USER_SUCCESS = 'LOAD_OTHER_USER_SUCCESS'
+export const LOAD_OTHER_USER_FAILURE = 'LOAD_OTHER_USER_FAILURE'
 
 export default (state = initialState, action) => {
   return produce(state, draft => {
@@ -38,6 +44,18 @@ export default (state = initialState, action) => {
         break
       }
       case LOAD_USER_FAILURE: {
+        break
+      }
+      case LOAD_OTHER_USER_REQUEST: {
+        draft.isLoddingOwner = true
+        break
+      }
+      case LOAD_OTHER_USER_SUCCESS: {
+        draft.userInfo = action.data
+        draft.isLoddingOwner = false
+        break
+      }
+      case LOAD_OTHER_USER_FAILURE: {
         break
       }
       default: {
--- client/sagas/user.js
+++ client/sagas/user.js
@@ -8,10 +8,13 @@ import {
   LOAD_USER_REQUEST,
   LOAD_USER_SUCCESS,
   LOAD_USER_FAILURE,
+  LOAD_OTHER_USER_REQUEST,
+  LOAD_OTHER_USER_SUCCESS,
+  LOAD_OTHER_USER_FAILURE,
 } from '../reducers/user'
 
 function loadUserApi() {
-  return axios.post('/user', {}, { withCredentials: true })
+  return axios.get('/user', { withCredentials: true })
 }
 
 function* loadUser(action) {
@@ -23,12 +26,38 @@ function* loadUser(action) {
     })
     yield action.data.resolve('success')
   } catch (e) {
+    yield put({
+      type: LOAD_USER_FAILURE,
+    })
     yield action.data.reject(e)
   }
 }
 
 function* watchLoadUser() {
   yield takeLatest(LOAD_USER_REQUEST, loadUser)
+}
+
+function loadOtherUserApi(userName) {
+  return axios.get(`/user/${userName}`)
+}
+
+function* loadOtherUser(action) {
+  try {
+    const userName = action.data
+    const result = yield call(loadOtherUserApi, userName)
+    yield put({
+      type: LOAD_OTHER_USER_SUCCESS,
+      data: result.data,
+    })
+  } catch (e) {
+    yield put({
+      type: LOAD_OTHER_USER_FAILURE,
+    })
+  }
+}
+
+function* watchLoadOtherUser() {
+  yield takeLatest(LOAD_OTHER_USER_REQUEST, loadOtherUser)
 }
 
 function signoutApi() {
@@ -54,7 +83,7 @@ function* watchSignout() {
 }
 
 function* userSaga() {
-  yield all([fork(watchLoadUser), fork(watchSignout)])
+  yield all([fork(watchLoadUser), fork(watchSignout), fork(watchLoadOtherUser)])
 }
 
 export default userSaga
