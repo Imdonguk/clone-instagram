967c5cce918ab608f911f8c6917c782fbe812320
Imdonguk <com8157@naver.com>
2019-11-18 23:56:22 +0900

    Add : Post컴포넌트에 썻던 하위 컴포넌트들 PostTemplate에서 재사용해서 렌더(state가 아닌 dummy데이터를 사용)

--- client/components/post/PostTemplate.js
+++ client/components/post/PostTemplate.js
@@ -1,12 +1,76 @@
 import React from 'react'
 import styled from 'styled-components'
+import { PostHeader, PostContent, PostPageCommentList, PostIcons, PostLikeCount, PostInputForm } from './index'
 
+const dummyData = {
+  user: {
+    userName: 'woogieboogie',
+    name: '우기부기',
+    image: {
+      src: 'IMG_24531573541982446.jpg',
+    },
+  },
+
+  images: [{ id: 1, src: 'IMG_24531573541982446.jpg' }],
+  description: '하이욱!',
+  id: 1,
+  likers: [1, 2, 3],
+}
 const PostTemplate = () => {
-  return <Wrap />
+  return (
+    <Wrap>
+      <PostContentWrap>
+        <PostContent img={dummyData.images} />
+      </PostContentWrap>
+      <PostRightLayout>
+        <PostHeaderWrap>
+          <PostHeader user={dummyData.user} />
+        </PostHeaderWrap>
+        <PostCommentListWrap>
+          <PostPageCommentList user={dummyData.user} comments={[]} description={dummyData.description} />
+        </PostCommentListWrap>
+        <PostIcons postId={dummyData.id} likers={dummyData.likers} />
+        <PostLikeCount likers={dummyData.likers} />
+        <PostInputForm postId={dummyData.id} />
+      </PostRightLayout>
+    </Wrap>
+  )
 }
 
 const Wrap = styled.div`
-  padding-top: 6rem;
+  margin: 4rem 0;
+  display: flex;
+  height: 60rem;
+
+  border: 0.1rem solid #e6e6e6;
+`
+
+const PostContentWrap = styled.div`
+  width: 60rem;
+  height: inherit;
+`
+
+const PostRightLayout = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  height: inherit;
+  background-color: #fff;
+  padding: 0 1.6rem;
+  border-left: 0.1rem solid #e6e6e6;
+`
+
+const PostHeaderWrap = styled.div`
+  padding: 1.6rem 0;
+  border-bottom: 0.1rem solid #e6e6e6;
+`
+
+const PostCommentListWrap = styled.div`
+  flex: 1;
+  overflow: auto;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `
 
 export default PostTemplate
