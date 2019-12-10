da7f04eeee12ef988f2f12c1a0a2415ca565b96f
Imdonguk <com8157@naver.com>
2019-12-10 17:06:17 +0900

    Setting : 배포용으로 웹팩 설정 ( 배포시에 gzip으로 파일압축 )

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,30 @@
 const withCSS = require('@zeit/next-css')
 const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+const CompressionPlugin = require('compression-webpack-plugin')
 
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    const prod = process.env.NODE_ENV === 'production'
+    const plugins = [...config.plugins]
+    prod && plugins.push(new CompressionPlugin())
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'eval',
+      module: {
+        ...config.module,
+      },
+      output: {
+        ...config.output,
+      },
+      plugins,
+    }
+  },
+}
 
 module.exports = withPlugins(
   [
@@ -13,6 +35,7 @@ module.exports = withPlugins(
       },
     ],
     [withCSS, { cssModules: true }],
+    withBundleAnalyzer,
   ],
   nextConfig,
 )
