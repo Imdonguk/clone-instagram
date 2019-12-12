51a7d386c162dbadb3f649d7f0ae11cfce9bb873
Imdonguk <com8157@naver.com>
2019-12-12 23:10:29 +0900

    Modify : 컴포넌트 분리 및 까먹은 key값 입력

--- client/components/popover/PreviewImg.js
+++ client/components/popover/PreviewImg.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { memo } from 'react'
 import styled from 'styled-components'
 import { useSelector, useDispatch } from 'react-redux'
 import { REMOVE_IMAGE_REQUEST } from '../../reducers/post'
 
-const PreviewImg = () => {
+const PreviewImg = memo(() => {
   const { imagePaths } = useSelector(state => state.post)
+  return <Wrap>{imagePaths && imagePaths.map(v => <PreviewImageComponent src={v} key={v} />)}</Wrap>
+})
+
+const PreviewImageComponent = memo(({ src }) => {
   const dispatch = useDispatch()
   const handleDeletePreviewImg = value => () => {
     dispatch({
@@ -13,19 +17,14 @@ const PreviewImg = () => {
     })
   }
   return (
-    <Wrap>
-      {imagePaths &&
-        imagePaths.map(v => (
-          <div>
-            <img src={v} alt="preview" />
-            <button type="button" onClick={handleDeletePreviewImg(v)}>
-              제거
-            </button>
-          </div>
-        ))}
-    </Wrap>
+    <div>
+      <img src={src} alt="preview" />
+      <button type="button" onClick={handleDeletePreviewImg(src)}>
+        제거
+      </button>
+    </div>
   )
-}
+})
 
 const Wrap = styled.div`
   display: flex;
