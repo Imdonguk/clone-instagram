9c6599c3d1076581779bc8ec01366c004ea82d47
Imdonguk <com8157@naver.com>
2020-01-02 23:57:25 +0900

    Modify : 게시물페이지 접속할 때 항상 LOAD_POST 액션사이클 실행하도록 변경 및 UserListPopover컴포넌트 이용

--- client/pages/post.js
+++ client/pages/post.js
@@ -3,8 +3,8 @@ import Helmet from 'react-helmet'
 import { useSelector } from 'react-redux'
 import Layout from '../components/layout'
 import { PostTemplate } from '../components/post'
-import { LOAD_POST_REQUEST, LOAD_POST_SUCCESS, LOAD_COMMENTS_REQUEST } from '../reducers/post'
-import { CancleFollow } from '../components/popover'
+import { LOAD_POST_REQUEST, LOAD_COMMENTS_REQUEST } from '../reducers/post'
+import { CancleFollow, UserListPopover } from '../components/popover'
 
 const Post = () => {
   const { post } = useSelector(state => state.post)
@@ -40,6 +40,7 @@ const Post = () => {
         <PostTemplate />
       </Layout>
       <CancleFollow />
+      <UserListPopover />
     </>
   )
 }
@@ -47,23 +48,14 @@ const Post = () => {
 Post.getInitialProps = async context => {
   const postState = context.store.getState()
   if (postState.post.post.id && postState.post.post.id === +context.query.id) return null
-  const post =
-    postState.post.posts.find(v => v.id === +context.query.id) ||
-    postState.post.hashtagPosts.find(v => v.id === +context.query.id) ||
-    postState.post.userPosts.find(v => v.id === +context.query.id) ||
-    false
 
-  if (post) {
-    context.store.dispatch({ type: LOAD_POST_SUCCESS, data: post })
-  } else {
-    await new Promise((resolve, reject) =>
-      context.store.dispatch({
-        type: LOAD_POST_REQUEST,
-        data: context.query.id,
-        promise: { resolve, reject },
-      }),
-    )
-  }
+  await new Promise((resolve, reject) =>
+    context.store.dispatch({
+      type: LOAD_POST_REQUEST,
+      data: context.query.id,
+      promise: { resolve, reject },
+    }),
+  )
 
   await new Promise((resolve, reject) =>
     context.store.dispatch({
