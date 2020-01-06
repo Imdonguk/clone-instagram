a195a3aa37d501008066c6a9dc3c6c5c49ba986d
Imdonguk <com8157@naver.com>
2020-01-06 17:38:51 +0900

    Add : NextButton, PrevButton 클릭했을 때 다음게시물 이전게시물 로드하는 핸들러함수 생성 ( 현재 pathname에 따라 post값 userPosts일지 hashtagPosts일 지 분기처리 )

--- client/components/popover/PostPopover.js
+++ client/components/popover/PostPopover.js
@@ -1,23 +1,78 @@
-import React from 'react'
+import React, { useEffect, useMemo } from 'react'
 import styled from 'styled-components'
-import { useSelector } from 'react-redux'
+import { useRouter } from 'next/router'
+import { useSelector, useDispatch } from 'react-redux'
 import { PopoverWrap } from './PopoverStyle'
 import PostTemplate from '../post/PostTemplate'
 import { NextButton, PrevButton } from '../Icons'
+import {
+  LOAD_POST_REQUEST,
+  LOAD_COMMENTS_REQUEST,
+} from '../../reducers/post'
 
 const UserPostPopover = () => {
+  const dispatch = useDispatch()
+  const router = useRouter()
   const { isPostPopover } = useSelector(state => state.popover)
+  const hasMorePost = useSelector(state => state.post.hasMorePost)
+  const posts =
+    router.pathname === '/user'
+      ? useSelector(state => state.post.userPosts)
+      : useSelector(state => state.post.hashtagPosts)
+
+  const postId = useSelector(state => state.post.post.id)
+  const currentPostIndex = posts.findIndex(v => v.id === postId)
+  const isPrevPost = useMemo(() => posts[currentPostIndex - 1], [postId])
+  const isNextPost = useMemo(() => posts[currentPostIndex + 1] || hasMorePost, [postId])
+
+  const handleClickNextButton = async () => {
+    if (isNextPost) {
+      const nextPostId = posts[currentPostIndex + 1].id
+      await new Promise((resolve, reject) =>
+        dispatch({
+          type: LOAD_POST_REQUEST,
+          postId: nextPostId,
+          promise: { resolve, reject },
+        }),
+      )
+
+      await new Promise((resolve, reject) =>
+        dispatch({
+          type: LOAD_COMMENTS_REQUEST,
+          postId: nextPostId,
+          promise: { resolve, reject },
+        }),
+      )
+    }
+  }
+
+  const handleClickPrevButton = async () => {
+    if (isPrevPost) {
+      const prevPostId = posts[currentPostIndex - 1].id
+      await new Promise((resolve, reject) =>
+        dispatch({
+          type: LOAD_POST_REQUEST,
+          postId: prevPostId,
+          promise: { resolve, reject },
+        }),
+      )
+
+      await new Promise((resolve, reject) =>
+        dispatch({
+          type: LOAD_COMMENTS_REQUEST,
+          postId: prevPostId,
+          promise: { resolve, reject },
+        }),
+      )
+    }
+  }
 
   if (!isPostPopover) return null
   return (
     <PopoverWrap>
-      <ArrowButtonWrap>
-        <PrevButton />
-      </ArrowButtonWrap>
+      <ArrowButtonWrap>{isPrevPost && <PrevButton onClick={handleClickPrevButton} />}</ArrowButtonWrap>
       <PostTemplate />
-      <ArrowButtonWrap>
-        <NextButton />
-      </ArrowButtonWrap>
+      <ArrowButtonWrap>{isNextPost && <NextButton onClick={handleClickNextButton} />}</ArrowButtonWrap>
     </PopoverWrap>
   )
 }
